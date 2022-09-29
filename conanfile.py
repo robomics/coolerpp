695d45e75bad4fd3ca82b0d0209d444c55c03c83
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-09-29 17:49:48 +0200

    Revert f4df89ca135dbd3d9fd920f94ec58bc86ff0a82b
    It turns out it is not a good idea to pin the recipe revision in the
    conanfile.py of a library, as it forces library users to pass the
    --update flag to conan install.

--- conanfile.py
+++ conanfile.py
@@ -15,14 +15,14 @@ class Coolerpp(ConanFile):
     license = "MIT"
     author = "Roberto Rossini (roberros@uio.no)"
     settings = "os", "compiler", "build_type", "arch"
-    requires = ["catch2/3.1.0@#e6317a112d286cae5f349f02d5cda905",
-                "fast_float/3.5.1@#63ccdfa6e4dbc05de4bc598258b6a12f",
-                "fmt/9.1.0@#78313935d914fe65715ad39687445de6",
-                "hdf5/1.12.2@#b01e96ebe1e351ee1d65ae49a347c29c",
-                "highfive/2.4.1@#0be5e237f353ec719b2abcc4ada9d9dd",
-                "tsl-hopscotch-map/2.3.0@#497d3f41172cefe2df9ac17692c52734",
-                "tsl-ordered-map/1.0.0@#dceec5bd0243a5c659939d0295dfee50",
-                "zlib/1.2.12@#a30750797caa71bd61bd0a18189caa28"]
+    requires = ["catch2/3.1.0",
+                "fast_float/3.5.1",
+                "fmt/9.1.0",
+                "hdf5/1.12.2",
+                "highfive/2.4.1@",
+                "tsl-hopscotch-map/2.3.0",
+                "tsl-ordered-map/1.0.0",
+                "zlib/1.2.12"]
 
     generators = "cmake", "cmake_find_package", "cmake_find_package_multi"
 
